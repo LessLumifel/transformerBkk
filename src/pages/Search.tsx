5a26e7a61a07eac4763b6a07bd55b5ffb76c64ff
Less Lumifel <110042563+LessLumifel@users.noreply.github.com>
2023-06-07 08:36:57 +0700

    Create Search.tsx

---
+++ src/pages/Search.tsx
@@ -0,0 +1,13 @@
+import AppBars from '../componants/AppBars'
+import TransformerMap from '../componants/Map'
+
+const Search = () => {
+  return (
+  <>
+    <AppBars/>
+    <TransformerMap/>
+    </>
+  )
+}
+
+export default Search
