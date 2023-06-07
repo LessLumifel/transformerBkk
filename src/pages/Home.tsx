1f59604670151c2542432ffdc37b41ee0b111c0d
Less Lumifel <110042563+LessLumifel@users.noreply.github.com>
2023-06-07 08:37:01 +0700

    Create Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import AppBars from '../componants/AppBars'
+import TransformerMap from '../componants/Map'
+
+const Home = () => {
+  return (
+  <>
+    <AppBars/>
+    <TransformerMap/>
+    </>
+  )
+}
+
+export default Home
