fc12e83f2b1aac16a1acf52e9e255b310b820fd9
Less Lumifel <110042563+LessLumifel@users.noreply.github.com>
2023-06-07 08:37:04 +0700

    Create About.tsx

---
+++ src/pages/About.tsx
@@ -0,0 +1,13 @@
+import AppBars from '../componants/AppBars'
+import TransformerMap from '../componants/Map'
+
+const About = () => {
+  return (
+  <>
+    <AppBars/>
+    <TransformerMap/>
+    </>
+  )
+}
+
+export default About
