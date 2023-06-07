9d184871c8ba07a59aeb0c38452efb4827aa56c6
Less Lumifel <110042563+LessLumifel@users.noreply.github.com>
2023-06-07 08:37:02 +0700

    Create Contact.tsx

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,13 @@
+import AppBars from '../componants/AppBars'
+import TransformerMap from '../componants/Map'
+
+const Contact = () => {
+  return (
+  <>
+    <AppBars/>
+    <TransformerMap/>
+    </>
+  )
+}
+
+export default Contact
